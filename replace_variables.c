5a7ff4290dfc08c1ffd6eac9d1fd8fbd53007a62
Awe Ayomidipupo <pupoawe@gmail.com>
2022-11-13 12:04:20 +0100

    refactor: use global implementation of _getenv

--- replace_variables.c
+++ replace_variables.c
@@ -4,17 +4,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-char *_getenv(char *var, char **env);
-
 /**
  * replace_variables - replaces variables in a shell program denoted by $
  * @args: array strings containing the command and its arguments
  * @status: exit status of previously executed program
- * @env: Array of environment variables
  *
  * Return:
 */
-void replace_variables(char **args, int status, char **env)
+void replace_variables(char **args, int status)
 {
 	int i = 1;
 	char *str;
@@ -37,7 +34,7 @@ void replace_variables(char **args, int status, char **env)
 				break;
 
 			default:
-				str = _getenv(&args[i][1], env);
+				str = _getenv(&args[i][1]);
 				break;
 			}
 
@@ -53,42 +50,4 @@ void replace_variables(char **args, int status, char **env)
 
 }
 
-/**
- * _getenv - gets the value of an environment variable
- * @var: name of environment variable
- * @env: array of environment variables
- *
- * Return: malloc'd string containing value of the variable
- * or NULL on error
-*/
-char *_getenv(char *var, char **env)
-{
-	int i = 0, len;
-	char *str;
 
-	if (!env || !var)
-		return (NULL);
-
-	while (env[i])
-	{
-		len = _strlen(var);
-
-		/* the environment varible exists */
-		if (_strncmp(var, env[i], len) == 0)
-		{
-			/*
-			* Copy variable value to new malloc'd memory,
-			* copy only after the = sign e.g PATH=WIE -> WIE
-			 */
-			str = _strdup(&env[i][len + 1]);
-
-			if (!str)
-				return (NULL);
-
-			return (str);
-		}
-		i++;
-	}
-
-	return (NULL);
-}
